4493244759bf8d6fc270e7741d08d9d5cd2f4ac8
Hannes Karppila <hannes.karppila@gmail.com>
2017-04-21 08:51:18 +0300

    Fix tests

--- tests/unit/test_compress.py
+++ tests/unit/test_compress.py
@@ -3,16 +3,19 @@ import base64
 
 import mod_manager
 
-mm = mod_manager.ModManager()
-mp = mm.get_pack("test")
-mp.save()
-compressed = mp.compress()
-data = base64.b64decode(compressed)
+def test_compress():
+    import time
 
-name, *lines = data.decode().split("\n")
+    mm = mod_manager.ModManager()
+    mp = mm.get_pack("test")
+    mp.save()
+    compressed = mp.compress()
+    data = base64.b64decode(compressed)
 
-assert name == "test"
-assert mp.path.endswith("modpacks/test.txt")
+    name, *lines = data.decode().split("\n")
 
-with open(mp.path) as f:
-    assert lines == [line.strip() for line in f.readlines()]
+    assert name == "test"
+    assert mp.path.endswith("modpacks/test.txt")
+
+    with open(mp.path) as f:
+        assert lines == [line.strip() for line in f.readlines()]
--- tests/unit/test_list.py
+++ tests/unit/test_list.py
@@ -1,6 +1,7 @@
 import os
 import mod_manager
 
-mm = mod_manager.ModManager()
+def test_list():
+    mm = mod_manager.ModManager()
 
-assert [mp.name for mp in mm.modpacks] == [fname.rsplit(".",1)[0] for fname in os.listdir("modpacks")]
+    assert [mp.name for mp in mm.modpacks] == [fname.rsplit(".",1)[0] for fname in os.listdir("modpacks")]
